76b059161dcad40e6141ef71c9621e8ab1dc4222
Christophe <christophe.moana.barth@gmail.com>
2024-12-09 12:05:18 +0100

    wilders

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+
+const theo = "Théophile";
+const moun = "Mounir";
+const cla = "Clarelle";
+const clem = "Clément";
+
+console.log(chalk.green(theo) + " " + chalk.red(moun) + " " + chalk.yellow(cla) + " " + chalk.blue(clem));
+
